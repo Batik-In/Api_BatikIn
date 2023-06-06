22aa53eddf5fbe72c88fb50146e4b8295882bff0
satriowicaksn <satrioputro564@gmail.com>
2023-06-07 00:58:30 +0700

    Create controller and flow for Quiz

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,14 +2,13 @@ import exresss from 'express';
 import authRouter from './auth.router';
 import articleRouter from './article.router';
 import classificationRouter from './classification.router';
+import quizRouter from './quiz.router';
 
 const router = exresss.Router();
 
 router.use('/api/auth', authRouter);
 router.use('/api/articles', articleRouter);
 router.use('/api/classification', classificationRouter);
-
-/* TODO */
-router.use('/api/quiz', authRouter);
+router.use('/api/quiz', quizRouter);
 
 export default router;
